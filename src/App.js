c3e6842e1ae153757e766942e7775fc16ce6c319
nasteho bashir <nasteho764@gmail.com>
2023-07-19 04:57:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import Header from "./Components/Header";
 import Search from "./Components/Search";
 import MovieCard from "./Components/MovieCard";
+import { useEffect, useState } from "react";
 function App() {
+
+  const [card, setCard] = useState([])
+
+  function movie(name) {
+    fetch(`https://api.tvmaze.com/search/shows?q=${name}`)
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        setCard(data)
+      })
+  }
+
+  useEffect(() => {
+    movie()
+    // console.log("hello");
+  }, [])
+
+
   return (
-    <div classNameName="App">
+    <div className="App">
       <Header />
       <div className="main">
-        <Search />
+
+        <Search search={movie} />
         <div className="movies-section">
           {/* <!-- one card --> */}
-          <MovieCard />
+          {card.map((cards) => <MovieCard data={cards} />)}
+
+          {/* <MovieCard /> */}
           {/* <!-- one card --> */}
         </div>
       </div>
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,10 +1,21 @@
-function Search() {
+import { useState } from "react";
+
+function Search(props) {
+  const [search, setSearch] = useState("")
   return (
     <div className="search-section">
       Search for your favorite shows
-      <div classNameName="search-input">
-        <input className="search-box" type="text" placeholder="Search" />
-        <button className="btn-search">Search</button>
+      <div className="search-input">
+        <input
+          className="search-box"
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button
+          className="btn-search"
+          onClick={() => props.search(search)}
+        >Search</button>
       </div>
     </div>
   );
